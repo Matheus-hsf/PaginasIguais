2a7e10b56f0091f48a774dcaa3ca07b1c17ca185
DrRin <math2099@yahoo.com.br>
2020-06-18 01:09:23 -0300

    Base do projeto

---
+++ PaginasNaoFeitas/Pessoa.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaginasNaoFeitas
+{
+    class Pessoa
+    {
+        private string nome;
+
+    }
+}
